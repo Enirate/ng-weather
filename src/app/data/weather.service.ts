77dff6ccb75b1d3c71471a9ff0d756fb0219ab72
Enirate <enirate@gmail.com>
2017-02-27 15:10:52 +0100

    House cleaning

--- src/app/data/weather.service.ts
+++ src/app/data/weather.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 import'rxjs/Rx' //operator/map'
 
 @Injectable()
 export class WeatherService {
-  //openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1
-//openweathermap.org/data/2.5/weather?id=2172797&appid=b1b15e88fa797225412429c1c50c122a1
-  baseUrl:string = 'http://api.openweathermap.org/data/2.5/';  //weather?id'; '//2352776&APPID=';
+  baseUrl:string = 'http://api.openweathermap.org/data/2.5/';
   apiKey:string = 'd3df4fe732bc2fa23481b8dcbd1d64e2'
 
   constructor(private _http:Http) { }
